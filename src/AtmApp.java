da371e1d2193f98086eb216407592a31d45a03d8
Ebubekir <ebubekirc442@gmail.com>
2023-02-22 21:36:01 +0300

    vs3

--- src/AtmApp.java
+++ src/AtmApp.java
@@ -6,5 +6,7 @@ public class AtmApp {
         System.out.println("This is a Atm Project!!");
         //version2
         System.out.println("java is easy");
+        //version 3
+        System.out.println("java çok zor");
     }
 }
